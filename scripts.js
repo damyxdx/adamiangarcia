81eb8e5adf124f3b7e7cdd4f862e22319d0bd867
damyxdx <damyxdx12@gmail.com>
2024-11-11 11:40:43 -0300

    "Removed unnecessary code from scripts.js and fixed newline at end of index.html"

--- scripts.js
+++ scripts.js
@@ -18,24 +18,4 @@ document.addEventListener("DOMContentLoaded", function() {
             window.open(link.href, '_blank');
         });
     });
-
-  document.addEventListener("DOMContentLoaded", function() {
-    const commands = document.querySelectorAll(".command");
-    let delay = 0;
-
-    commands.forEach((command) => {
-        delay += 300;
-        setTimeout(() => {
-            command.style.opacity = 1;
-        }, delay);
-    });
-
-    const links = document.querySelectorAll("a");
-    links.forEach(link => {
-        link.addEventListener("click", (e) => {
-            e.preventDefault();
-            window.open(link.href, '_blank');
-        });
-    });
-
-   
+});
